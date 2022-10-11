e709e5593d7c2e547c980f43df0700dd65c80ccb
gloxounet <matteo.glock@laposte.net>
2022-10-11 17:43:16 +0200

    tp4

---
+++ TP4.hs
@@ -0,0 +1,170 @@
+-- NOM : GLOCK
+-- PRENOM : Matteo
+
+-- respectez les formes demandees :
+
+-- [rec def]: une definition recursive, une fonction qui s'appelle elle meme
+-- [non rec def]: une definition non recursive, une fonction qui ne s'appelle pas elle meme
+-- [ZF def]: une definition avec une liste en comprehension
+
+-- fonctions auxiliaires autorisees : (==), (/=), (+), (<=), (:), (.), (++), filter, foldr, length, map
+
+-- compression de huffman
+
+exemple1 :: String
+exemple1 = "abcbcaa"
+
+-- arbre de Huffman
+
+data Huff a = Leaf a | Node (Huff a) (Huff a) deriving (Show, Eq)
+
+-- chemin dans un arbre
+
+type Bits = [Bit]
+
+data Bit = L | R deriving (Show, Eq)
+
+-- construction de l'arbre de Huffman
+
+-- Q1 [rec def]
+-- calcule pour chaque caractere d'une chaine son nombre d'occurences
+
+testQ1 :: Bool
+testQ1 = nbOccurrences exemple1 == [('a', 3), ('b', 2), ('c', 2)]
+
+nbOccurrences :: String -> [(Char, Int)]
+nbOccurrences (h:q) = let n = length q + 1
+                          qf = filter (/= h) q
+                      in (h,n - length qf):nbOccurrences qf
+nbOccurrences [] = []
+
+-- Q2 [rec def]
+-- le poids est la somme des occurences contenues dans ses feuilles
+
+testQ2 :: Bool
+testQ2 = poids (Node (Leaf ('a', 3)) (Node (Leaf ('b', 2)) (Leaf ('c', 2)))) == 7
+
+poids :: Huff (a, Int) -> Int
+poids (Node g d) = poids g + poids d
+poids (Leaf (_,n)) = n
+
+-- Q3 [rec def]
+-- insere un arbre a sa place dans une liste d'arbres tries par poids croissants
+
+testQ3 :: Bool
+testQ3 = insere (Leaf ('a', 3)) [Leaf ('b', 2), Leaf ('c', 2)] == [Leaf ('b', 2), Leaf ('c', 2), Leaf ('a', 3)]
+
+insere :: Huff (a, Int) -> [Huff (a, Int)] -> [Huff (a, Int)]
+insere a (h:q) | ph < pa = h:insere a q
+               | ph == pa = a:h:q
+               | otherwise = h:a:q
+               where ph = poids h
+                     pa = poids a
+insere a [] = [a]
+
+-- Q4 [non rec def avec foldr]
+-- tri des arbres de Huffman par poids croissants
+
+testQ4 :: Bool
+testQ4 = triHuff [Leaf ('a', 3), Leaf ('b', 2), Leaf ('c', 2)] == [Leaf ('b', 2), Leaf ('c', 2), Leaf ('a', 3)]
+
+triHuff :: [Huff (a, Int)] -> [Huff (a, Int)]
+triHuff = foldr (\x -> \fxs -> insere x fxs) []
+
+-- Q5 [non rec def]
+-- construit la liste des feuilles pour une chaine donnee
+
+testQ5 :: Bool
+testQ5 = feuilles exemple1 == [Leaf ('b', 2), Leaf ('c', 2), Leaf ('a', 3)]
+
+feuilles :: String -> [Huff (Char, Int)]
+feuilles str = triHuff (map Leaf (nbOccurrences str))
+
+
+-- Q6 [rec def]
+-- agreege les deux arbres les plus legers, repetitivement pour obtenir un arbre unique
+
+testQ6 :: Bool
+testQ6 = agrege [Leaf ('b', 2), Leaf ('c', 2), Leaf ('a', 3)] == Node (Leaf ('a', 3)) (Node (Leaf ('b', 2)) (Leaf ('c', 2)))
+
+agrege :: [Huff (a, Int)] -> Huff (a, Int)
+agrege (h1:h2:q) = agrege (insere (Node h1 h2) q)
+agrege [h1] = h1
+agrege [] = error "Liste vide"
+
+-- Q7 [rec def]
+-- efface les nombre d'occurences dans un arbre de Huffman
+
+testQ7 :: Bool
+testQ7 = strip (Node (Leaf ('a', 3)) (Node (Leaf ('b', 2)) (Leaf ('c', 2)))) == Node (Leaf 'a') (Node (Leaf 'b') (Leaf 'c'))
+
+strip :: Huff (a, Int) -> Huff a
+strip (Node g d) = Node (strip g) (strip d)
+strip (Leaf (s,_)) = Leaf s
+
+-- construit l'arbre de Huffman pour une chaine donnee
+
+test' :: Bool
+test' = buildHuff exemple1 == Node (Leaf 'a') (Node (Leaf 'b') (Leaf 'c'))
+
+buildHuff :: String -> Huff Char
+buildHuff = strip . agrege . feuilles
+
+-- codage d'une chaine
+
+-- code un caractere
+
+test'' :: Bool
+test'' = codeOne (Node (Leaf 'a') (Node (Leaf 'b') (Leaf 'c'))) 'b' == [R, L]
+
+codeOne :: Huff Char -> Char -> Bits
+codeOne root c = head (codeOne' root c)
+
+-- Q8 [def recursive qui utilise aussi map]
+-- code un caractere, retourne une liste de longueur 1 qui contient le chemin du caractere
+-- commencer par ecrire la fonction qui retourne tous les chemins puis la modifier
+
+testQ8 :: Bool
+testQ8 = codeOne' (Node (Leaf 'a') (Node (Leaf 'b') (Leaf 'c'))) 'b' == [[R, L]]
+
+codeOne' :: Huff Char -> Char -> [Bits]
+codeOne' (Node g d) c = map (L:) (codeOne' g c) ++ map (R:) (codeOne' d c)
+codeOne' (Leaf a) c | a==c =  [[]]
+                    | a/=c = []
+
+-- code tous les caracteres d'une chaine
+
+test''' :: Bool
+test''' = codeAll (Node (Leaf 'a') (Node (Leaf 'b') (Leaf 'c'))) exemple1 == [L, R, L, R, R, R, L, R, R, L, L]
+
+codeAll :: Huff Char -> String -> Bits
+codeAll root s = concat (map (codeOne root) s)
+
+-- Q9 [rec def]
+-- decodage d'une liste de bits
+
+testQ9 :: Bool
+testQ9 = decode (Node (Leaf 'a') (Node (Leaf 'b') (Leaf 'c'))) (Node (Leaf 'a') (Node (Leaf 'b') (Leaf 'c'))) [L, R, L, R, R, R, L, R, R, L, L] == "abcbcaa"
+
+decode :: Huff Char -> Huff Char -> Bits -> String
+decode a1 (Leaf a) l = a:(decode a1 a1 l)
+decode a1 (Node g d) (h:q) | h == R = decode a1 d q
+                           | h == L = decode a1 g q
+decode _ _ [] = []
+
+-- verifie la correction en codange puis decodant et calcule le ratio de compression (hors arbre)
+
+test'''' :: Bool
+test'''' = test10 exemple1 == (True, 0.19642857)
+
+test10 :: String -> (Bool, Float)
+test10 s =
+  let t = buildHuff s
+      c = codeAll t s
+      s' = decode t t c
+   in (s == s', (fromIntegral (length c)) / (fromIntegral (8 * length s)))
+
+-- Q10
+-- en 10 mots maximum identifier ce qui est recalcule de nombreuses fois et pourrait etre optimise dans ce code
+
+-- reponse : Stocker poids dans noeud (cf. TP3)
